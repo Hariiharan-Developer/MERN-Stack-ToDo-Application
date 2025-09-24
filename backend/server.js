47f71cf59a8445c01aabb6499dee5fca3e41d4ea
Hariharan.s <hreehs1428@gmail.com>
2025-09-24 17:27:14 +0530

    Database connected

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,11 @@
 const express =require('express')
 const router = require('./route/todo.router')
+const connectDB = require('./config/cofig')
 const app = express()
 const dotenv  =require('dotenv').config()
 
 
-
+connectDB()
 app.use('/api/todo',router)
 app.listen(process.env.PORT,()=>{
     console.log(`server listening on the port:http://localhost:${process.env.PORT}`)
